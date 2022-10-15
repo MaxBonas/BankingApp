1fc8304296fa3aec9ecc0c262396ecef11da35a7
Max Bonas <113984950+MaxBonas@users.noreply.github.com>
2022-10-15 15:31:36 +0200

    Account Entities preDone

--- src/main/java/PaloosaBank/OnlineBanking/entities/accounts/CreditCard.java
+++ src/main/java/PaloosaBank/OnlineBanking/entities/accounts/CreditCard.java
@@ -1,13 +1,58 @@
 package PaloosaBank.OnlineBanking.entities.accounts;
 
+import PaloosaBank.OnlineBanking.embedables.Money;
+import PaloosaBank.OnlineBanking.entities.users.AccountHolder;
+import PaloosaBank.OnlineBanking.enums.Status;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 public class CreditCard extends Account{
 
+    private Money creditLimit = new Money(BigDecimal.valueOf(250));
 
+    private Money penaltyFee = new Money(BigDecimal.valueOf(40));
 
+    private double interestRate = 12;
+
+    public CreditCard(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner,
+                      LocalDate creationDate, Status status, Money creditLimit, Money penaltyFee, double interestRate) {
+        super(balance, primaryOwner, secondaryOwner, creationDate, status);
+        this.creditLimit = creditLimit;
+        this.penaltyFee = penaltyFee;
+        this.interestRate = interestRate;
+    }
+
+    public CreditCard() {
+
+    }
+
+    public Money getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(Money creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public Money getPenaltyFee() {
+        return penaltyFee;
+    }
+
+    public void setPenaltyFee(Money penaltyFee) {
+        this.penaltyFee = penaltyFee;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
 }
--- src/main/java/PaloosaBank/OnlineBanking/entities/accounts/Savings.java
+++ src/main/java/PaloosaBank/OnlineBanking/entities/accounts/Savings.java
@@ -17,12 +17,29 @@ public class Savings extends Account{
     private Money minimumBalance = new Money(BigDecimal.valueOf(1000));
     private double interestedRate = 0.0025; //Max 0,5
 
-    public Savings(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, LocalDate creationDate, Status status, Money minimumBalance, double interestedRate) {
+    public Savings(Money balance, AccountHolder primaryOwner, AccountHolder secondaryOwner, LocalDate creationDate,
+                   Status status, Money minimumBalance, double interestedRate) {
         super(balance, primaryOwner, secondaryOwner, creationDate, status);
         this.minimumBalance = minimumBalance;
         this.interestedRate = interestedRate;
     }
 
     public Savings() {
+    }
+
+    public Money getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    public void setMinimumBalance(Money minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
+    public double getInterestedRate() {
+        return interestedRate;
+    }
+
+    public void setInterestedRate(double interestedRate) {
+        this.interestedRate = interestedRate;
     }
 }
